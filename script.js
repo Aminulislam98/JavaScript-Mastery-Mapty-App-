bccfbbb7dd18d71352c35c6163d6704586f64fbe
Aminul islam <aminurrohman98@gmail.com>
2026-07-24 08:50:08 +0100

    Add Leaflet library for map display and update geolocation handling

--- script.js
+++ script.js
@@ -17,8 +17,23 @@ navigator.geolocation.getCurrentPosition(
     const { latitude, longitude } = position.coords;
     console.log(latitude, longitude);
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    // Display a map using leaflet library :
+    const cords = [latitude, longitude];
+    const map = L.map('map').setView(cords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(cords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     alert('Could not get your location.');
   },
 );
+console.log(name);
