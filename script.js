f511be03a52fbdde93a06bf638a32522393a8eb1
Aminul islam <aminurrohman98@gmail.com>
2026-07-25 13:51:46 +0100

    Refactor map marker popup to enhance customization and maintainability

--- script.js
+++ script.js
@@ -27,20 +27,22 @@ navigator.geolocation.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(cords)
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-      });
-
     map.on('click', function (mapEvent) {
       console.log(mapEvent);
       const { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          }),
+        )
+        .setPopupContent('Workout')
+        .openPopup();
     });
   },
   function () {
